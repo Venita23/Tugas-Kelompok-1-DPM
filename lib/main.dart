c14861883f2931e73776c622b21c09132427b285
Wenita Amelia <wenitaamelia5@gmail.com>
2025-07-11 10:46:10 +0700

    pengaturan akun

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'profile_page.dart';
 import 'genre_page.dart';
 import 'bacaan_page.dart';
 import 'home_controller.dart'; // ✅ Tambahkan ini
-import 'pengaturan_akun.dart';
 
 
 void main() {
