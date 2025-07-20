4a0adc394e6038a13635d3a222e16eb687433a5c
Niren <nirenren2004@gmail.com>
2025-07-20 12:59:26 +0700

    update sipnosis

--- lib/detail_bacaan_page.dart
+++ lib/detail_bacaan_page.dart
@@ -181,23 +181,3 @@ Padding(
   }
 }
 
-class _StatBox extends StatelessWidget {
-  final IconData icon;
-  final String label;
-
-  const _StatBox({required this.icon, required this.label});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Icon(icon, size: 20),
-        const SizedBox(height: 4),
-        Text(
-          label,
-          style: const TextStyle(fontSize: 13),
-        ),
-      ],
-    );
-  }
-}
